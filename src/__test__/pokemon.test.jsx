8f5e093c1d4e1c779c3a9bbdde9edc510574df17
clement-jny <82049263+clement-jny@users.noreply.github.com>
2023-03-01 16:08:13 +0100

    Installing vitest and setup test

---
+++ src/__test__/pokemon.test.jsx
@@ -0,0 +1,19 @@
+/**
+ * @vitest-environment jsdom
+ */
+
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { Pokemons } from "../components/pokemons";
+
+test("je vérifie que le premier pokémon apparait", async () => {
+	console.log(Pokemons)
+	render(
+		<MemoryRouter>
+			<Pokemons />
+		</MemoryRouter>
+	);
+	//const linkElement = screen.getByText(/bulbasaur/i);
+	const $poke = await screen.findByText("Bulbasaur");
+	expect($poke).toBeInTheDocument();
+})
